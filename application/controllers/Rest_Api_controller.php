14bf083f51bc7e418f29da66bffa079b24bf9525
sundarraj.p <sundarraj@usistech.com>
2022-06-14 18:33:54 +0530

    updated

--- application/controllers/Rest_Api_controller.php
+++ application/controllers/Rest_Api_controller.php
@@ -47,6 +47,27 @@ class Rest_Api_controller extends CI_Controller{
 
 	}
 
+	public function curl_demo(){
+
+		// From URL to get webpage contents.
+		// $url = "https://www.geeksforgeeks.org/";
+			$url=base_url();
+
+		// Initialize a CURL session.
+		$ch = curl_init();
+
+		// Return Page contents.
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+		//grab URL and pass it to the variable.
+		curl_setopt($ch, CURLOPT_URL, $url);
+
+		$result = curl_exec($ch);
+
+		echo $result;
+
+	}
+
 }
 
 ?>
